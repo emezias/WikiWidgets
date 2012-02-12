c2848ffe8b98178f82a2a29e78dcd24c1c779d89
Elizabeth Mezias <emezias@gmail.com>
2012-02-12 00:58:55 -0800

    changed package names

--- src/com/mezcode/GeoItem.java
+++
@@ -1,6 +0,0 @@
-package com.mezcode;
-
-public class GeoItem extends PicItem {
-	double latitude;
-	double longitude;
-}
--- src/com/mezcode/MapList.java
+++
@@ -1,7 +0,0 @@
-package com.mezcode;
-import android.app.ListFragment;
-
-
-public class MapList extends ListFragment {
-
-}
